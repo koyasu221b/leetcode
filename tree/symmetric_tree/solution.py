8d53d5ecc7fa1e6616df3afba4189a1610ec4c35
will <will@willdeMacBook-Air.local>
2015-06-20 17:47:26 +0800

    update 3 invert, same, symmetric solutions

---
+++ tree/symmetric_tree/solution.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    # @param {TreeNode} root
+    # @return {boolean}
+    def isSymmetric(self, root):
+        if root is None:
+            return True
+        return self.isSame(root.left, root.right)
+
+    def isSame(self, left, right):
+        if left is None and right is None:
+            return True
+        if left is None or right is None:
+            return False
+        if left.val != right.val:
+            return False
+        return self.isSame(left.left, right.right) and self.isSame(left.right, right.left)
